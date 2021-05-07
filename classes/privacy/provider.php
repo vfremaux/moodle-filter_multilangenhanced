cce3212fabbc9ae986c747b3693caffe74906e04
Valery Fremaux <valery.fremaux@gmail.com>
2021-05-07 16:49:49 +0200

    Automated sync from SVN

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -18,7 +18,14 @@ namespace filter_multilangenhanced\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Privacy Subsystem for filter_enhancedmultilang implementing null_provider.
+ *
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class provider implements \core_privacy\local\metadata\null_provider {
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
